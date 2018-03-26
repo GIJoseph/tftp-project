674f051727a75d918a13df14f4cf9fec747ab127
Joseph Goergen <jgoergen15@winona.edu>
2018-03-26 10:47:15 -0500

    progress

--- src/tftpclient.java
+++ src/tftpclient.java
@@ -6,16 +6,47 @@ import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class tftpclient {
 
+	byte opcode;
+	String fileName = "beatUpSteven.txt";
+	String serverAddress = "10.19.104.44";
 	public static void main(String[] args) throws IOException {
 		//String fileName = "TFTP.pdf";
 		//tftpclient tFTPClientNet = new tftpclient();
-		InetAddress net = InetAddress.getByName("10.19.104.44");
-		System.out.println(net.toString());
+		
 	}
-	public void getFile() {
-		DatagramPacket dp = new DatagramPacket(null, 0);
+	public void readFile() throws IOException {
+		opcode = 1;
+		InetAddress InetServerAddress = InetAddress.getByName(serverAddress);
+		DatagramSocket udpPacketSender = new DatagramSocket();
+		byte[] packet = requestPacketByteArray(opcode, fileName);
+		DatagramPacket packetToSend = new DatagramPacket(packet, packet.length, InetServerAddress, 69);
+		udpPacketSender.send(packetToSend);
+		
+		
+		udpPacketSender.get
+	}
+	public byte[] requestPacketByteArray(byte opcode, String fileName) {
+		String mode = "octet";
+		byte[] result = new byte[2 + fileName.length() + 1 + mode.length() + 1];
+		int i = 0;
+		result[i] = 0;
+		i++;
+		result[i] = opcode;
+		i++;
+		for(int k = 0; k < fileName.length(); k++,i++) {
+			result[i] = (byte) fileName.charAt(k);
+		}
+		result[i] = 0;
+		for (int k = 0; k < mode.length(); k++,i++) {
+			result[i] = (byte) mode.charAt(k);
+		}
+		result[i] = 0;
+		
+		return result;
 	}
 }
